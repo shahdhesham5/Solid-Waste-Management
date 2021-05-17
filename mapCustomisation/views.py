a0868367527dd11bf69ef9f0f17453e961089d08
peng22 <>
2021-05-17 16:40:30 +0200

    fix some issues with mapcustomisation

--- mapCustomisation/views.py
+++ mapCustomisation/views.py
@@ -213,95 +213,103 @@ def index2(request):
 
 
 import requests
-
 @login_required
 def index(request):
-    cat = Catalog("http://localhost/geoserver/rest/", username="admin", password="geoserver")
-    cat2= gs_catalog
-    # shapefile_plus_sidecars = geoserver.util.shapefile_and_friends('cat.get_layer("states")')
-    # myResource = cat.get_resources()
+    cat = Catalog("https://localhost/geoserver/rest/", username="admin", password="geoserver")
     try:
         all_layers = cat.get_layers()
     except:
-        # layersFromJson=requests.get('http://localhost/geoserver/rest/layers/').json()
         cat2= gs_catalog
         all_layers = cat2.get_layers()
+
+    layers={}
+    for layer in all_layers:
+        try:
+            jsonLayer=(requests.get('https://localhost/geoserver/wfs?request=GetFeature&service=WFS&version=1.0.0&typeName='+layer.name+'&srsName=EPSG:4326&outputFormat=application/json').json())
+        except:
+            continue
+        layers[layer.name]=jsonLayer
+    url='https://localhost/geoserver/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geonode:states&maxFeatures=50&outputFormat=json'
+    context={
+        # "layerw":that_layer,
+        "layers":all_layers,
+        # "shape":shapefile_plus_sidecars['shp'],
+        "link":url,
+        # "layersGeojson":geoJsonLayer,
+        'url':url,
+        'geolayers':json.dumps(layers)
+    }
+
+
+    return render(request,'da/index.html',context)
+
+#first feature of the shapefile
+            # context_dict = {}
+            # bb = cat2.get_layer(layer.name)
+            # x = bb.read()
+            # x = json.loads(x)
+            # data_dict = layer.name#json.loads(request.POST.get('json_data'))
+            # layername = layer.name
+            # filtered_attributes = ''
+            # if not isinstance(data_dict['filtered_attributes'], string_types):
+            #     filtered_attributes = [x for x in data_dict['filtered_attributes'] if '/load_layer_data' not in x]
+            # name = layername if ':' not in layername else layername.split(':')[1]
+            # location = "{location}{service}".format(** {
+            #     'location': settings.OGC_SERVER['default']['LOCATION'],
+            #     'service': 'wms',
+            # })
+            # access_token = None
+            # if request and 'access_token' in request.session:
+            #     access_token = request.session['access_token']
+            # if access_token and 'access_token' not in location:
+            #     location = '%s?access_token=%s' % (location, access_token)
+            # try:
+            #     wfs = WebFeatureService(
+            #         location,
+            #         version='1.1.0')
+            #     response = wfs.getfeature(
+            #         typename=layername,
+            #         outputFormat='application/json')
+            #     return HttpResponse("No")
+            #
+            #     x = response.read()
+            #     x = json.loads(x)
+            #
+            #     features_response = json.dumps(x)
+            #     decoded = json.loads(features_response)
+            #
+            #     decoded_features = decoded['features']
+            #     properties = {}
+            #     for key in decoded_features[0]['properties']:
+            #         properties[key] = []
+            #
+            #     # loop the dictionary based on the values on the list and add the properties
+            #     # in the dictionary (if doesn't exist) together with the value
+            #     from collections.abc import Iterable
+            #     for i in range(len(decoded_features)):
+            #         for key, value in decoded_features[i]['properties'].items():
+            #             if value != '' and isinstance(value, (string_types, int, float)) and (
+            #                     (isinstance(value, Iterable) and '/load_layer_data' not in value) or value):
+            #                 properties[key].append(value)
+            #
+            #     for key in properties:
+            #         properties[key] = list(set(properties[key]))
+            #         properties[key].sort()
+            #
+            #     context_dict["feature_properties"] = properties
+            # except Exception:
+            #     x=1
+            #     traceback.print_exc()
+            #     logger.error("Possible error with OWSLib.")
+            #
+            # jsonLayer=x
+            #
+            #json.dumps(context_dict)
+            # continue
         # return HttpResponse(all_layers[0])
 
         # print(layersFromJson)
         # all_layers= layersFromJson.layers.layer
-    layers={}
-    for layer in all_layers:
-        try:
-            jsonLayer=(requests.get('http://localhost/geoserver/wfs?request=GetFeature&service=WFS&version=1.0.0&typeName='+layer.name+'&srsName=EPSG:4326&outputFormat=application/json').json())
-        except:
-            continue
-            context_dict = {}
-            bb = cat2.get_layer(layer.name)
-            x = bb.read()
-            x = json.loads(x)
-            data_dict = layer.name#json.loads(request.POST.get('json_data'))
-            layername = layer.name
-            filtered_attributes = ''
-            if not isinstance(data_dict['filtered_attributes'], string_types):
-                filtered_attributes = [x for x in data_dict['filtered_attributes'] if '/load_layer_data' not in x]
-            name = layername if ':' not in layername else layername.split(':')[1]
-            location = "{location}{service}".format(** {
-                'location': settings.OGC_SERVER['default']['LOCATION'],
-                'service': 'wms',
-            })
-            access_token = None
-            if request and 'access_token' in request.session:
-                access_token = request.session['access_token']
-            if access_token and 'access_token' not in location:
-                location = '%s?access_token=%s' % (location, access_token)
-            try:
-                wfs = WebFeatureService(
-                    location,
-                    version='1.1.0')
-                response = wfs.getfeature(
-                    typename=layername,
-                    outputFormat='application/json')
-                return HttpResponse("No")
-
-                x = response.read()
-                x = json.loads(x)
-
-                features_response = json.dumps(x)
-                decoded = json.loads(features_response)
-
-                decoded_features = decoded['features']
-                properties = {}
-                for key in decoded_features[0]['properties']:
-                    properties[key] = []
-
-                # loop the dictionary based on the values on the list and add the properties
-                # in the dictionary (if doesn't exist) together with the value
-                from collections.abc import Iterable
-                for i in range(len(decoded_features)):
-                    for key, value in decoded_features[i]['properties'].items():
-                        if value != '' and isinstance(value, (string_types, int, float)) and (
-                                (isinstance(value, Iterable) and '/load_layer_data' not in value) or value):
-                            properties[key].append(value)
-
-                for key in properties:
-                    properties[key] = list(set(properties[key]))
-                    properties[key].sort()
-
-                context_dict["feature_properties"] = properties
-            except Exception:
-                x=1
-                traceback.print_exc()
-                logger.error("Possible error with OWSLib.")
-
-            jsonLayer=x
-            #
-            #json.dumps(context_dict)
-            # continue
-        layers[layer.name]=jsonLayer
-
-
-
     # that_layer = cat2.get_layer("states")
     # print(type(that_layer))
     # resource = that_layer.resource#cat.get_resource(that_layer.name)
@@ -310,7 +318,6 @@ def index(request):
     # print("sddd",resource.keys())
     # resource = that_layer.resource
     # url='http://localhost/geoserver/wfs?request=GetFeature&service=WFS&version=1.0.0&typeName=states&srsName=EPSG:4326&outputFormat=application/json'
-    url='http://localhost/geoserver/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geonode:states&maxFeatures=50&outputFormat=json'
     # response = requests.get(url)
     # data = json.loads(response.read())
     # r = requests.get(url)
@@ -325,19 +332,3 @@ def index(request):
 #  , '_set_attr_attribution', '_set_default_style', 'clear', 'fetch', 'message', 'refresh', 'serialize']
 
     # print (that_layer.dom.__dict__.keys())
-
-
-
-    context={
-        # "layerw":that_layer,
-        "layers":all_layers,
-        # "shape":shapefile_plus_sidecars['shp'],
-        "link":url,
-        # "layersGeojson":geoJsonLayer,
-        'url':url,
-        'geolayers':json.dumps(layers)
-    }
-
-    return render(request,'da/index.html',context)
-
-#first feature of the shapefile
