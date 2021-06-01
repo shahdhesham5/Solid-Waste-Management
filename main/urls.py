2c468defa6c7f360454fea35cdeb94116de008b1
peng22 <>
2021-06-01 07:02:30 +0200

    improving the main app

--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
  path('', views.main, name='main'),
  path('mainLogin', views.login, name='mainLogin'),
+ path('<int:pk>',views.country_projects,name='country_projects'),
+ path('projects/<int:pk>',views.project_details,name='project_details'),
+ path('mainLogout',views.mainLogout,name='mainLogout'),
+
  ]
--- main/views.py
+++ main/views.py
@@ -1,20 +1,66 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from django.contrib.auth import get_user_model
-from django.contrib.auth import authenticate ,login as auth_login
+from django.contrib.auth import (authenticate ,
+                                 login as auth_login,
+                                 logout )
 from django.contrib import messages
 
 from django.urls import reverse ,reverse_lazy
 import json
 User = get_user_model()
-
+from .models import (Project_Team,
+                    Project_Country,
+                    Project,
+                    Project_task)
 # Create your views here.
 def main(request):
     if request.user.is_authenticated :
-        return render(request,'main/main.html')
+        try:
+            project_teams=request.user.project_teams.all()
+            print('project_teams',len(project_teams))
+            for team in project_teams:
+                countries=team.project_locations.all()
+                print('country',len(countries))
+            context={
+                    'countries':countries
+                    }
+        except:
+            context={
+                'countries':[]
+                    }
+            
+
+
+        return render(request,'main/countries.html',context)
     else:
         return render(request,'main/login.html')
-        return HttpResponse('login')
+
+
+def country_projects(request,pk):
+    country=Project_Country.objects.get(id=pk)
+    projects=country.projects.all()
+    context={
+        'projects':projects,
+        'country':country
+    }
+    return render(request,'main/country_projects.html',context)
+
+def project_details(request,pk):
+    project=Project.objects.get(id=pk)
+    tasks=project.Project_tasks.all()
+    completed_tasks=project.Project_tasks.filter(completed=True)
+    progress=(len(completed_tasks)/len(tasks))*100
+
+    context={
+        'project':project,
+        'tasks':tasks,
+        'completed_tasks':completed_tasks,
+        'progress':progress
+        }
+    return render(request,'main/project_details.html',context)
+
+
 
 
 def login(request):
@@ -34,3 +80,8 @@ def login(request):
 
     return HttpResponse('h')
     pass
+
+
+def mainLogout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse_lazy('main'))
